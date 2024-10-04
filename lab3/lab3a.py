1d8373fce9799f77bb54ed1a4d51abece2b17ec4
Nirmal Gautam <neermal4311@gmail.com>
2024-10-03 22:29:55 -0400

    Add lab3 scripts and output file

---
+++ lab3/lab3a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: ngautam11
+def return_text_value():
+    return "Good Morning Terry"  # Keep this as it is
+
+# return_number_value() function
+def return_number_value():
+    return 15  # Change this to return 15 directly
+
+# Main Program
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
